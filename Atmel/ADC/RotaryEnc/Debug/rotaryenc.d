28107fcb03cfb09ac78dac6751936115a1d33266
Reece Williams <rw11g12@soton.ac.uk>
2015-04-15 16:05:10 +0100

    Rotary encoder working with different warning levels

--- Atmel/ADC/RotaryEnc/Debug/rotaryenc.d
+++ Atmel/ADC/RotaryEnc/Debug/rotaryenc.d
@@ -12,6 +12,8 @@ rotaryenc.d rotaryenc.o: .././rotaryenc.c \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h \
  .././rotaryenc.h .././uart.h
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\io.h:
@@ -39,6 +41,10 @@ c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-g
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
 c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\atmel\ toolchain\avr8\ gcc\native\3.4.1056\avr8-gnu-toolchain\lib\gcc\avr\4.8.1\include\stddef.h:
 
 .././rotaryenc.h:
 
--- Atmel/ADC/RotaryEnc/rotaryenc.c
+++ Atmel/ADC/RotaryEnc/rotaryenc.c
@@ -7,6 +7,7 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>
 #include "rotaryenc.h"
 #include "uart.h"
 
@@ -54,7 +55,7 @@ static uint8_t two_bpos = 0;
 
 void rot_init()
 {
-		RTR_DDR |= ~pins; //Set Port C as inputs
+		RTR_DDR |= ~pins; //Set Port A as inputs
 		RTR_PORT |= pins; // Enable pull up resistors
 		
 		//PCICR = _BV(PCIE0); //Enable interrupts on Port B
@@ -63,10 +64,10 @@ void rot_init()
 		DDRD |= _BV(PIND3);			//Rotary Select Pin	
 		PORTD |= _BV(PIND3);
 			
-		tmp_two = (RTR_PIN & pins);		// Initial encoder status (2)
+		//tmp_two = (RTR_PIN & pins);		// Initial encoder status (2)
 		
-		PORTD &= ~_BV(PIND3);
-		tmp_one = (RTR_PIN & pins);		
+		//PORTD &= ~_BV(PIND3);
+		//tmp_one = (RTR_PIN & pins);		
 		
 		int fill = 0;
 		for(fill = 0; fill <= 255; fill++)	// Set all invalid en
@@ -77,7 +78,7 @@ void rot_init()
 			
 }
 
-ISR(PCINT0_vect)
+ISR(PCINT0_vect)		//NOT USED- NO NEED
 {
 	uart_str("Button interrupt\n");
 	if(!sel){
@@ -89,25 +90,28 @@ ISR(PCINT0_vect)
 		tmp_two = (RTR_PIN & pins);
 	}
 }
+
 uint8_t rtr_qpos(uint8_t x)
 {
-	if(!x){								//Retrieves encoder one position (0-128)
+	if(!x){								
 		PORTD |= _BV(PIND3);
-		if (waiting_one){
-			rtr_pos_one = rtrlut_fast[rtr_value()];
-			return rtrlut_fast[rtr_value()];
-		}
 		
+		if (rtrlut_fast[rtr_value()] == 255){	//Retrieves encoder one position (0-128)
+			uart_str("ENCODER ERROR! \n");		//SEND ERROR incorrect reading
+			return rtr_pos_one;
+		}
+		rtr_pos_one = rtrlut_fast[rtr_value()];
+			
 		return rtr_pos_one;
 	}
-	else if(x){							//Retrieves encoder two position (0-128)
+	else if(x){							
 		PORTD &= ~_BV(PIND3);
-		if (waiting_two)
-			if (waiting_one){
-				rtr_pos_two = rtrlut_fast[rtr_value()];
-				return rtrlut_fast[rtr_value()];
-			}
-			
+		
+		if (rtrlut_fast[rtr_value()] == 255){	//Retrieves encoder two position (0-128)
+			uart_str("ENCODER ERROR! \n");		//SEND ERROR incorrect reading
+			return rtr_pos_two;
+		}
+		rtr_pos_two = rtrlut_fast[rtr_value()];
 		
 		return rtr_pos_two;
 	}
@@ -121,7 +125,7 @@ uint8_t rtr_value()
 	return RTR_PIN & pins;
 }
 
-uint8_t rtr_position(uint8_t x)
+uint8_t rtr_position(uint8_t x)		//NOT USED- SLOWER VERSION
 {
 	if(!x){								//Retrieves encoder one position (0-128)
 		PORTD |= _BV(PIND3);
@@ -167,8 +171,153 @@ uint8_t rtr_position(uint8_t x)
 	return RTR_INVALID;
 }
 
+void rtr_speed()
+{
+	int speed_acc_one = 0;		//Accumulate RE changes
+	int speed_acc_two = 0;
+	uint8_t index = 0;			//Difference buffer access index \\ Buffer size 75
+	
+	static uint8_t speed_warn_11 = 0;		//To signify levels of danger // Number AB stand for Encoder Number : Warning Level
+	static uint8_t speed_warn_buff_11 = 0;
+	static uint8_t speed_warn_12 = 0;
+	static uint8_t speed_warn_buff_12 = 0;
+	
+	static uint8_t speed_warn_21 = 0;		//To signify levels of danger // Number AB stand for Encoder Number : Warning Level
+	static uint8_t speed_warn_buff_21 = 0;
+	static uint8_t speed_warn_22 = 0;
+	static uint8_t speed_warn_buff_22 = 0;
+
+	uint8_t acc;		//25 positions per second = speed over a second
+	for(acc = 0; acc <= 25; acc++){
+		if(one_bpos-acc <= -1)
+			index = 74 - (acc-one_bpos); //Deal with wrap around
+		else
+			index = one_bpos-acc;
+		if(two_bpos-acc <= -1)
+			index = 74 - (acc-two_bpos);
+		else
+			index = two_bpos-acc;
+
+		if((index < 0) | (index >74))		//Check for out of bounds access
+			uart_str("INDEX ERROR");
+
+		speed_acc_one += cbuf_one_dif[index];
+		speed_acc_two += cbuf_two_dif[index];
+	}
+	
+	if(abs(speed_acc_one) > 16)
+		speed_warn_11 = 1;
+	else
+		speed_warn_11 = 0;
+		
+	if(abs(speed_acc_one) > 32)
+		speed_warn_12 = 1;
+	else
+		speed_warn_12 = 0;
+		
+	if(!speed_warn_buff_11 && speed_warn_11)
+		uart_str("Rotary encoder 1 SPEED WARNING(1)\n");
+	else if (speed_warn_buff_11 && !speed_warn_11)
+		uart_str("Rotary encoder 1 SPEED CLEAR(1)\n");
+		
+	if(!speed_warn_buff_12 && speed_warn_12)
+		uart_str("Rotary encoder 1 SPEED WARNING(2)\n");
+	else if (speed_warn_buff_12 && !speed_warn_12)
+		uart_str("Rotary encoder 1 SPEED CLEAR(2)\n");	
+	
+	if(abs(speed_acc_two) > 16)
+		speed_warn_21 = 1;
+	else
+		speed_warn_21 = 0;
+		
+	if(abs(speed_acc_two) > 32)
+		speed_warn_22 = 1;
+	else
+		speed_warn_22 = 0;
+		
+	if(!speed_warn_buff_21 && speed_warn_21)
+		uart_str("Rotary encoder 2 SPEED WARNING(1)\n");
+	else if (speed_warn_buff_21 && !speed_warn_21)
+		uart_str("Rotary encoder 2 SPEED CLEAR(1)\n");
+		
+	if(!speed_warn_buff_22 && speed_warn_22)
+		uart_str("Rotary encoder 2 SPEED WARNING(2)\n");
+	else if (speed_warn_buff_22 && !speed_warn_22)
+		uart_str("Rotary encoder 2 SPEED CLEAR(2)\n");
+
+	
+	speed_warn_buff_11 = speed_warn_11;
+	speed_warn_buff_12 = speed_warn_12;
+	speed_warn_buff_21 = speed_warn_21;
+	speed_warn_buff_22 = speed_warn_22;
+}
+
+
+void rtr_speed_update()		//CURRENTLY NOT USED -> MAYBE DETECT BLIPS?
+{
+	static int speed_update_acc_one = 0;
+	static int speed_update_acc_two = 0;
+	
+	static int speed_counter_one = 0;
+	static int speed_counter_two = 0;
+	
+	static uint8_t speed_flag_one = 0;
+	static uint8_t speed_flag_buff = 0;
+	static uint8_t speed_flag_two = 0;
+	
+	if(cbuf_one_dif[one_bpos] != 0){
+		//if((cbuf_one_dif[one_bpos] < -10) | (cbuf_one_dif[one_bpos] > 10)){
+			//uart_str("WARNING!");
+			//uart_number(abs(cbuf_one_dif[one_bpos]));
+			//uart_str("\n");
+		//}
+		speed_update_acc_one += cbuf_one_dif[one_bpos];
+		speed_counter_one = 0;
+	}
+	else if((cbuf_one_dif[one_bpos] == 0) && (speed_update_acc_one > 0)){	//Will set the accumulator back to zero & allows for 'cool off' period
+		if((speed_update_acc_one - (1*speed_counter_one)) < 0)
+			speed_update_acc_one = 0;
+		else		
+			speed_update_acc_one -= (1*speed_counter_one);
+	}
+	else if((cbuf_one_dif[one_bpos] == 0) && (speed_update_acc_one < 0)){	//Will set the accumulator back to zero & allows for 'cool off' period
+		speed_update_acc_one += 1;
+	}
+	//else if((cbuf_one_dif[one_bpos] == 0) && (speed_counter_one = 3) && (speed_update_acc_one != 0)){	// Will set the accumulator back to zero & allows for 'cool off' period
+	//speed_update_acc_one -= -3;
+	//speed_counter_one = 0;
+	//}
+	
+	if((speed_update_acc_one > 32) | (speed_update_acc_one < -32))
+	speed_flag_one = 1;
+	else if((speed_update_acc_one < 32) && (speed_update_acc_one > -32))
+	speed_flag_one = 0;
+	
+	
+	//if(speed_update_acc_one < 0){		//Test Output 
+		//uart_str("-");
+		//uart_number(abs(speed_update_acc_one));
+	//}
+	//else uart_number(speed_update_acc_one);
+	//uart_str("		");
+	
+	//if(cbuf_one_dif[one_bpos] !=0){
+		//uart_number(cbuf_one_dif[one_bpos]);
+		//uart_str("				");
+	//}
+	if((speed_flag_buff == 0) &&(speed_flag_one))
+	uart_str("Rot 1 Warn");
+	else if((speed_flag_buff == 1) && (!speed_flag_one))
+	uart_str("Rot 1 Clear");
+	
+	speed_counter_one++;
+	speed_flag_buff = speed_flag_one;
+	
+}
+
 void rtr_buff_update()
 {
+	//uart_str("Entered buff\n");
 	uint8_t im1;
 	uint8_t jm1;
 
@@ -183,118 +332,44 @@ void rtr_buff_update()
 		jm1 = two_bpos-1;
 
 	if(!sel){
-		cbuf_one[one_bpos] = rtr_qpos(sel);					//Update Cbuf and Cdif 
-		if((cbuf_one[one_bpos] <2) && (cbuf_one[im1] >126))
-			cbuf_one_dif[one_bpos] = 1;
-		else if((cbuf_one[one_bpos] >126) && (cbuf_one[im1] <2))
-			cbuf_one_dif[one_bpos] = -1;
+		cbuf_one[one_bpos] = rtr_pos_one;					//Update Cbuf and Cdif 
+		if((cbuf_one[one_bpos] <28) && (cbuf_one[im1] >100)){ // Dealing with wrap around in circular buffer
+			cbuf_one_dif[one_bpos] = (cbuf_one[one_bpos] + (128 - cbuf_one[im1]));
+			//uart_str("Wrapped");							//DEBUG
+			//uart_number(abs(cbuf_one_dif[one_bpos]));
+			//uart_str("\n");
+		}
+		else if((cbuf_one[one_bpos] >100) && (cbuf_one[im1] <28)){
+			cbuf_one_dif[one_bpos] = -(cbuf_one[im1] + (128 - cbuf_one[one_bpos]));
+		}		
 		else
 			cbuf_one_dif[one_bpos] = cbuf_one[one_bpos] - cbuf_one[im1];
+		
+		rtr_speed();		// Called here to keep positional buffer location
 
 		if(one_bpos == 74)
-			one_bpos = 0;
+		one_bpos = 0;
 		else
-			one_bpos++;
+		one_bpos++;
 	}
 	else{
-		cbuf_two[two_bpos] = rtr_qpos(sel);
-		if((cbuf_two[two_bpos] <2) && (cbuf_two[jm1] >126))
-			cbuf_two_dif[two_bpos] = 1;
-		else if((cbuf_two[two_bpos] >126) && (cbuf_two[jm1] <2))
-			cbuf_two_dif[two_bpos] = -1;
+		cbuf_two[two_bpos] = rtr_pos_two;
+		if((cbuf_two[two_bpos] <28) && (cbuf_two[jm1] >100))		// Dealing with wrap around in circular buffer
+			cbuf_two_dif[two_bpos] = (cbuf_two[two_bpos] + (128 - cbuf_two[jm1]));
+		else if((cbuf_two[two_bpos] >100) && (cbuf_two[jm1] <28))
+			cbuf_two_dif[two_bpos] = -(cbuf_two[jm1] + (128 - cbuf_two[two_bpos]));
 		else
 			cbuf_two_dif[two_bpos] = cbuf_two[two_bpos] - cbuf_two[jm1];
+		
+		rtr_speed();		// Called here to keep positional buffer location
 
 		if(two_bpos == 74)
 			two_bpos = 0;
 		else
 			two_bpos++;
-		//uart_number(cbuf_two_dif[jm1]);
-		//uart_str("\n");
 	}
-
-		
 }
 
-
-void rtr_speed()
-{
-	int speed_acc_one = 0;
-	int speed_acc_two = 0;
-	uint8_t index = 0;
-
-	uint8_t acc;		//25 positions per second = speed over a second
-	for(acc = 0; acc <= 25; acc++){
-		if(one_bpos-acc <= -1)
-			index = 74 - (acc-one_bpos);
-		else 
-			index = one_bpos-acc; 
-		if(two_bpos-acc <= -1)
-			index = 74 - (acc-two_bpos);
-		else
-			index = two_bpos-acc;
-
-		if((index < 0) | (index >74))		//Check for out of bounds access
-			uart_str("INDEX ERROR");
-			
-		speed_acc_one += cbuf_one_dif[index];
-		speed_acc_two += cbuf_two_dif[index];
-	}
-	//uart_number(speed_acc_one);
-	//uart_str("\r\n");
-	if(speed_acc_one > 32)
-		uart_str("Rotary encoder 1 WARNING\n");
-	if(speed_acc_two > 32)
-		uart_str("Rotary encoder 2 WARNING\n");
-}
-
-void rtr_speed_update()
-{
-	static int speed_update_acc_one = 0;
-	static int speed_update_acc_two = 0;
-	
-	static int speed_counter_one = 0;
-	static int speed_counter_two = 0;
-	
-	static uint8_t speed_flag_one = 0;
-	static uint8_t speed_flag_buff = 0;
-	static uint8_t speed_flag_two = 0;
-	
-	if(cbuf_one_dif[one_bpos] != 0){
-		speed_update_acc_one += cbuf_one_dif[one_bpos];
-		speed_counter_one = 0;
-	}
-	else if((cbuf_one_dif[one_bpos] == 0) && (speed_update_acc_one > 0)){	//Will set the accumulator back to zero & allows for 'cool off' period
-			speed_update_acc_one -= 1;
-	}
-	else if((cbuf_one_dif[one_bpos] == 0) && (speed_update_acc_one < 0)){	//Will set the accumulator back to zero & allows for 'cool off' period
-		speed_update_acc_one += 1;
-	}
-	//else if((cbuf_one_dif[one_bpos] == 0) && (speed_counter_one = 3) && (speed_update_acc_one != 0)){	// Will set the accumulator back to zero & allows for 'cool off' period
-		//speed_update_acc_one -= -3;
-		//speed_counter_one = 0;
-	//}
-	
-	if((speed_update_acc_one > 32) | (speed_update_acc_one < -32))
-		speed_flag_one = 1;
-	else if((speed_update_acc_one < 32) && (speed_update_acc_one > -32))
-		speed_flag_one = 0;
-	
-	//uart_number(speed_update_acc_one);
-	//uart_str("				");
-	if(cbuf_one_dif[one_bpos] !=0){
-	uart_number(cbuf_one_dif[one_bpos]);
-	uart_str("				");
-	}
-	//if(speed_flag_one)
-		//uart_str("Rot 1 Warn");
-	//else if((speed_flag_buff == 1) && (!speed_flag_one))
-		//uart_str("Rot 1 Clear");
-		
-	speed_counter_one++;
-	speed_flag_buff = speed_flag_one;
-	
-}
 
 void rtr_intrpt()
 {	
@@ -302,7 +377,7 @@ void rtr_intrpt()
 		static uint8_t buff_one = 0;		// Monitors if position has changed = easier to read information
 		rtr_pos_one = rtr_qpos(sel);
 		if(buff_one != rtr_pos_one){
-			//uart_str("\nRotary ");
+			//uart_str("\nRotary ");	//DEBUGGING
 			//uart_number(sel);
 			//uart_str(" Pos :");
 			//uart_number(rtr_pos_one);
@@ -312,7 +387,7 @@ void rtr_intrpt()
 		static uint8_t buff_two = 0;
 		rtr_pos_two = rtr_qpos(sel);
 		if(buff_two != rtr_pos_two){
-			//uart_str("\nRotary ");
+			//uart_str("\nRotary ");	//DEBUGGING
 			//uart_number(sel);
 			//uart_str(" Pos :");
 			//uart_number(rtr_pos_two);
@@ -321,11 +396,8 @@ void rtr_intrpt()
 	}
 	//uart_str("Checked encoder pos\n");
 	rtr_buff_update();
-		if(cbuf_one_dif[one_bpos] !=0){
-			uart_number(cbuf_one_dif[one_bpos]);
-			uart_str("\n");
-		}
-	//rtr_speed_update();
+
+
 	sel = !sel;
 }
 
